b869ae7b088b15164c4f421d20fb0f6f0146e280
Alonso del Arte <alonso.delarte@gmail.com>
2018-10-25 23:37:20 -0400

    Add files via upload

--- code-source/algebraics/QuadraticInteger.java
+++ code-source/algebraics/QuadraticInteger.java
@@ -3,19 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package algebraicintegercalculator;
+package algebraics;
 
 /**
  *
  * @author Aonso del Arte
  */
-public class QuadraticInteger implements AlgebraicInteger {
+public abstract class QuadraticInteger implements AlgebraicInteger {
 
     protected int regPartMult;
     protected int surdPartMult;
     protected QuadraticRing quadRing;
     protected int denominator;
     
+    public QuadraticRing getRing() {
+        return quadRing;
+    }
+    
+    /**
+     * Gets the denominator of the imaginary quadratic integer when represented 
+     * as a fraction in lowest terms.
+     * @return 2 only in the case of so-called "half-integers," always 1 
+     * otherwise. So, if {@link #getRing()}{@link 
+     * ImaginaryQuadraticRing#hasHalfIntegers() .hasHalfIntegers()} is false, 
+     * this getter should always return 1. For example, for 3/2 + sqrt(-7)/2, 
+     * this would be 2, for 3 + sqrt(-7) this would be 1. In the ring of the 
+     * Gaussian integers, this getter should always return 1. Even if -1 or -2 
+     * was successfully used as a denominator in the constructor, this getter 
+     * will still return 1 or 2.
+     */
+    public int getDenominator() {
+        return this.denominator;
+    }
+
     /**
      * Gives the algebraic degree of the algebraic integer. Should not be higher than 2.
      * @return 0 if the algebraic integer is 0, 1 if it's a purely real integer, 2 otherwise.
@@ -38,7 +58,7 @@ public class QuadraticInteger implements AlgebraicInteger {
      * @return Twice the real part
      */
     @Override
-    public int trace() {
+    public long trace() {
         if (quadRing.d1mod4 && denominator == 2) {
             return regPartMult;
         } else {
@@ -47,16 +67,16 @@ public class QuadraticInteger implements AlgebraicInteger {
     }
     
     /**
-     * Calculates the norm of the imaginary quadratic integer (real part plus real integer times square root of a negative integer)
-     * @return Square of the real part minus square of the imaginary part. May be 0 but never negative.
+     * Calculates the norm of the quadratic integer.
+     * @return Square of the "regular" part minus square of the surd part times radicand.
      */
     @Override
-    public int norm() {
+    public long norm() {
         int N;
         if (quadRing.d1mod4 && denominator == 2) {
-            N = (regPartMult * regPartMult + quadRing.absRadicand * surdPartMult * surdPartMult)/4;
+            N = (regPartMult * regPartMult - quadRing.radicand * surdPartMult * surdPartMult)/4;
         } else {
-            N = regPartMult * regPartMult + quadRing.absRadicand * surdPartMult * surdPartMult;
+            N = regPartMult * regPartMult - quadRing.radicand * surdPartMult * surdPartMult;
         }
         return N;
     }
@@ -66,8 +86,8 @@ public class QuadraticInteger implements AlgebraicInteger {
      * @return An array of three integers. If the algebraic integer is of degree 2, the array will be {norm, negative trace, 1}; if of degree 1, then {number, 1, 0}, and for 0, {0, 0, 0}.
      */
     @Override
-    public int[] minPolynomial() {
-        int[] coeffs = {0, 0, 0};
+    public long[] minPolynomial() {
+        long[] coeffs = {0, 0, 0};
         switch (algebraicDegree()) {
             case 0: 
                 break;
@@ -87,7 +107,7 @@ public class QuadraticInteger implements AlgebraicInteger {
     @Override
     public String minPolynomialString() {
         String polString = "";
-        int[] polCoeffs = minPolynomial();
+        long[] polCoeffs = minPolynomial();
         switch (algebraicDegree()) {
             case 0:
                 break;
@@ -121,5 +141,283 @@ public class QuadraticInteger implements AlgebraicInteger {
         }
         return polString;
     }
+    
+    public QuadraticInteger conjugate() {
+        QuadraticInteger conjug = null;
+        if (this instanceof ImaginaryQuadraticInteger) {
+            conjug = new ImaginaryQuadraticInteger(this.regPartMult, -this.surdPartMult, this.quadRing, this.denominator);
+        }
+        if (this instanceof RealQuadraticInteger) {
+            conjug = new RealQuadraticInteger(this.regPartMult, -this.surdPartMult, this.quadRing, this.denominator);
+        }
+        if (!(this instanceof ImaginaryQuadraticInteger) && !(this instanceof RealQuadraticInteger)) {
+            String exceptionMessage = this.getClass().getName() + " is not a supported number domain for this function.";
+            exceptionMessage = exceptionMessage + "\nIt may be necessary to override QuadraticInteger.conjugate().";
+            throw new UnsupportedNumberDomainException(exceptionMessage, this, this);
+        }
+        return conjug;
+    }
+    
+    public abstract double abs();
+    
+    public abstract double getRealPartMultNumeric();
+    
+    public abstract double getImagPartwRadMultNumeric();
+    
+    /**
+     * A text representation of the quadratic integer, with the "regular" part  
+     * first and the "surd" part second.
+     * @return A String representing the imaginary quadratic integer which can 
+     * be used in a JTextField. Because of the "&radic;" character, this might 
+     * not be suitable for console output.
+     */
+    @Override
+    public String toString() {
+        String QIString = "";
+        if (this.denominator == 2) {
+            QIString = this.regPartMult + "/2 ";
+            if (this.surdPartMult < -1) {
+                QIString += (("- " + ((-1) * this.surdPartMult)) + "\u221A(" + this.quadRing.radicand + ")/2");
+            }
+            if (this.surdPartMult == -1) {
+                QIString += ("- \u221A(" + this.quadRing.radicand + ")/2");
+            }
+            if (this.surdPartMult == 1) {
+                QIString += ("+ \u221A(" + this.quadRing.radicand + ")/2");
+            }
+            if (this.surdPartMult > 1) {
+                QIString += ("+ " + this.surdPartMult + "\u221A(" + this.quadRing.radicand + ")/2");
+            } 
+        } else {
+            if (this.regPartMult == 0) {
+                if (this.surdPartMult == 0) {
+                    QIString = "0";
+                } else {
+                    if (this.surdPartMult < -1 || this.surdPartMult > 1) {
+                        QIString = this.surdPartMult + "\u221A(" + this.quadRing.radicand + ")";
+                    }
+                    if (this.surdPartMult == -1) {
+                        QIString = "-\u221A(" + this.quadRing.radicand + ")";
+                    }
+                    if (this.surdPartMult == 1) {
+                        QIString = "\u221A(" + this.quadRing.radicand + ")";
+                    }
+                }
+            } else {
+                QIString = Integer.toString(this.regPartMult);
+                if (this.surdPartMult < -1) {
+                    QIString += ((" - " + ((-1) * this.surdPartMult)) + "\u221A(" + this.quadRing.radicand + ")");
+                }
+                if (this.surdPartMult == -1) {
+                    QIString += (" - \u221A(" + this.quadRing.radicand + ")");
+                }
+                if (this.surdPartMult == 1) {
+                    QIString += (" + \u221A(" + this.quadRing.radicand + ")");
+                }
+                if (this.surdPartMult > 1) {
+                    QIString += (" + " + this.surdPartMult + "\u221A(" + this.quadRing.radicand + ")");
+                }
+            }
+        }
+        if (this.quadRing.radicand == -1) {
+            QIString = QIString.replace("\u221A(-1)", "i");
+        }
+        return QIString;
+    }
+    
+    /**
+     * A text representation of the quadratic integer, using theta notation when 
+     * {@link #getRing()}{@link ImaginaryQuadraticRing#hasHalfIntegers() 
+     * .hasHalfIntegers()} is true.
+     * @return A String representing the quadratic integer which can be used in 
+     * a JTextField. If {@link #getRing()}{@link 
+     * ImaginaryQuadraticRing#hasHalfIntegers() .hasHalfIntegers()} is false, 
+     * this just returns the same String as {@link #toString()}. Note that 
+     * &omega; here is used strictly only to mean -1/2 + (&radic;-3)/2, but 
+     * &theta; can mean 1/2 + (&radic;<i>d</i>)/2 for any <i>d</i> congruent to 
+     * 1 modulo 4 other than -3. Thus, for example, 5/2 + (&radic;-3)/2 = 3 + 
+     * &omega;, but 5/2 + (&radic;-7)/2 = 2 + &theta;, 5/2 + (&radic;-11)/2 = 2 
+     * + &theta;, 5/2 + (&radic;-15)/2 = 2 + &theta;, etc.
+     */
+    public String toStringAlt() {
+        String altQIString;
+        if (this.quadRing.d1mod4) {
+            int nonThetaPart = this.regPartMult;
+            int thetaPart = this.surdPartMult;
+            char thetaLetter = '\u03B8';
+            if (this.denominator == 1) {
+                nonThetaPart *= 2;
+                thetaPart *= 2;
+            }
+            if (this.quadRing.radicand == -3) {
+                nonThetaPart = (nonThetaPart + thetaPart)/2;
+                thetaLetter = '\u03C9'; // Now this holds omega instead of theta
+            } else {
+                nonThetaPart = (nonThetaPart - thetaPart)/2;
+            }
+            altQIString = Integer.toString(nonThetaPart);
+            if (nonThetaPart == 0 && thetaPart != 0) {
+                if (thetaPart < -1 || thetaPart > 1) {
+                    altQIString = Integer.toString(thetaPart) + thetaLetter;
+                }
+                if (thetaPart == -1) {
+                    altQIString = "-" + thetaLetter;
+                }
+                if (thetaPart == 1) {
+                    altQIString = Character.toString(thetaLetter);
+                }
+            } else {
+                if (thetaPart < -1) {
+                    altQIString += (" - " + ((-1) * thetaPart) + thetaLetter);
+                }
+                if (thetaPart == -1) {
+                    altQIString += (" - " + thetaLetter);
+                }
+                if (thetaPart == 1) {
+                    altQIString += (" + " + thetaLetter);
+                }
+                if (thetaPart > 1) {
+                    altQIString += (" + " + thetaPart + thetaLetter);
+                }
+            }
+        } else {
+            altQIString = this.toString();
+        }
+        return altQIString;
+    }
+    
+    /**
+     * A text representation of the quadratic integer using only ASCII 
+     * characters. All this function does is replace "\u221A" with "sqrt".
+     * @return A String using only ASCII characters. For example, for 
+     * "\u221A(-2)", the result will be "sqrt(-2)"; for "-3/2 + \u221A(37)/2", 
+     * the result would be "-3/2 + sqrt(37)/2".
+     */
+    @Override
+    public String toASCIIString() {
+        return this.toString().replace("\u221A", "sqrt");
+    }
+    
+    public String toASCIIStringAlt() {
+        return "Feature not implemented yet.";
+    }
+    
+    /**
+     * A text representation of the quadratic integer suitable for use in a TeX 
+     * document. If you prefer a single denominator instead of a separate 
+     * denominator for the "regular" and "surd" parts, use {@link 
+     * #toTeXStringSingleDenom()}. Although I have a unit test for this 
+     * function, I have not tested inserting the output of this function into an 
+     * actual TeX document.
+     * @return A String. For example, for 1/2 + sqrt(-7)/2, the result should be 
+     * "\frac{1}{2} + \frac{\sqrt{-7}}{2}".
+     */
+    @Override
+    public String toTeXString() {
+        if (this.quadRing.radicand == -1) {
+            return this.toString();
+        }
+        if (this.norm() == 0) {
+            return "0";
+        }
+        String QIString;
+        if (this.denominator == 1) {
+            if (this.regPartMult == 0) {
+                switch (this.surdPartMult) {
+                    case -1:
+                        QIString = "-\\sqrt{" + this.quadRing.radicand + "}";
+                        break;
+                    case 1:
+                        QIString = "\\sqrt{" + this.quadRing.radicand + "}";
+                        break;
+                    default:
+                        QIString = this.surdPartMult + " \\sqrt{" + this.quadRing.radicand + "}";
+                }
+            } else {
+                QIString = this.regPartMult + " + " + this.surdPartMult + " \\sqrt{" + this.quadRing.radicand + "}";
+                QIString = QIString.replace("+ -", " - ");
+                QIString = QIString.replace(" 1 \\sqrt", " \\sqrt");
+            }
+        } else {
+            QIString = "\\frac{" + this.regPartMult + "}{2} + \\frac{" + this.surdPartMult + " \\sqrt{" + this.quadRing.radicand + "}}{2}";
+            QIString = QIString.replace("\\frac{-", "-\\frac{");
+            QIString = QIString.replace("\\frac{1 \\sqrt", "\\frac{\\sqrt");
+            QIString = QIString.replace("+ -", " - ");
+        }
+        return QIString;
+    }
+    
+    /**
+     * A text representation of the quadratic integer suitable for use in a TeX 
+     * document, but only with a single denominator for both the "regular" and 
+     * "surd" parts, as opposed to {@link #toTeXString()}. Although I have a 
+     * unit test for this function, I have not tested inserting the output of 
+     * this function into an actual TeX document.
+     * @return A String. For example, for 1/2 + sqrt(-7)/2, the result should be 
+     * "\frac{1 + \sqrt{-7}}{2}".
+     */
+    public String toTeXStringSingleDenom() {
+        if (this.denominator == 2) {
+            String IQIString;
+            IQIString = "\\frac{" + this.regPartMult + " + " + this.surdPartMult + " \\sqrt{" + this.quadRing.radicand + "}}{2}";
+            IQIString = IQIString.replace(" 1 \\sqrt", " \\sqrt");
+            IQIString = IQIString.replace("+ -", " - ");
+            return IQIString;
+        } else {
+            return this.toTeXString();
+        }
+    }
+    
+    public String toTeXStringAlt() {
+        return "Feature not implemented yet.";
+    }
+    
+    @Override
+    public String toHTMLString() {
+        return "Feature not implemented yet.";
+    }
+    
+    public String toHTMLStringAlt() {
+        return "Feature not implemented yet.";
+    }
+    
+    // FEATURE NOT IMPLEMENTED YET
+    public static QuadraticInteger parseQuadraticInteger(QuadraticRing ring, String str) {
+        return null;
+    }
+    
+    // FEATURE NOT IMPLEMENTED YET
+    public static QuadraticInteger parseQuadraticInteger(String str) {
+        return null;
+    }
+    
+    /**
+     * Addition operation, since operator+ (plus) can't be overloaded.
+     * @param summand The quadratic integer to be added.
+     * @return A new RealQuadraticInteger or ImaginaryQuadraticInteger object with the result of the operation.
+     * @throws AlgebraicDegreeOverflowException If the algebraic integers come from different quadratic rings, the result of the sum will be an algebraic integer of degree 4 and this exception will be thrown.
+     */
+    public abstract QuadraticInteger plus(QuadraticInteger summand);
+    
+    public abstract QuadraticInteger plus(int summand);
+
+    /**
+     * Subtraction operation, since operator- can't be overloaded
+     * @param subtrahend The quadratic integer to be subtracted.
+     * @return A new RealQuadraticInteger or ImaginaryQuadraticInteger object with the result of the operation.
+     * @throws AlgebraicDegreeOverflowException If the algebraic integers come from different quadratic rings, the result of the sum will be an algebraic integer of degree 4 and this exception will be thrown.
+     */
+    public abstract QuadraticInteger minus(QuadraticInteger subtrahend);
+    
+    public abstract QuadraticInteger minus(int subtrahend);
+
+    public abstract QuadraticInteger times(QuadraticInteger multiplicand);
+    
+    public abstract QuadraticInteger times(int multiplicand);
+
+    public abstract QuadraticInteger divides(QuadraticInteger divisor);
+    
+    public abstract QuadraticInteger divides(int divisor) throws NotDivisibleException;
+
     
 }
--- code-source/algebraics/QuadraticRing.java
+++ code-source/algebraics/QuadraticRing.java
@@ -1,18 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2018 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package algebraicintegercalculator;
+package algebraics;
 
 /**
  *
  * @author Alonso del Arte
  */
-public class QuadraticRing implements IntegerRing {
+public abstract class QuadraticRing implements IntegerRing {
     
     /**
-     * Ought to be a squarefree negative integer
+     * Ought to be a squarefree integer
      */
     protected int radicand;
 
@@ -41,6 +52,18 @@ public class QuadraticRing implements IntegerRing {
         preferenceForBlackboardBold = preferenceForBB;
     }
     
+    public boolean hasHalfIntegers() {
+        return d1mod4;
+    }
+    
+    public int getRadicand() {
+        return radicand;
+    }
+    
+    public abstract int getAbsNegRad();
+    
+    public abstract double getAbsNegRadSqrt();
+    
     /**
      * A text representation of the ring. In some contexts, toTeXString() or toHTMLString() may be preferable.
      * @return A string representing the imaginary quadratic ring which can be output to the console.
@@ -67,11 +90,55 @@ public class QuadraticRing implements IntegerRing {
         }
         return IQRString;
     }
+    
+    /**
+     * A text representation of the ring's label. The main difference between 
+     * this function and toString() is that this function does not use the 
+     * "\u221A" character, using "sqrt" instead.
+     * @return A String representing the imaginary quadratic ring which can be 
+     * output to the console. Examples: for d = -7, the result is 
+     * "O_(Q(sqrt(-7)))"; for d = -5, the result is "Z[sqrt(-5)]"; for d = -3, 
+     * the result is "Z[omega]"; for d = 2, the result is "Z[sqrt(2)]"; for d = 
+     * 5, the result is "Z[phi]". Then you don't have to worry about the ability 
+     * to display the "\u221A", "\u03C9" and "\u03C6" characters.
+     */
+    @Override
+    public String toASCIIString() {
+        String IQRString;
+        switch (this.radicand) {
+            case -3:
+                IQRString = "Z[omega]"; // omega = -1/2 + sqrt(-3)/2 is a complex cubic root of 1
+                break;
+            case -1:
+                IQRString = "Z[i]"; // i is the imaginary unit, sqrt(-1)
+                break;
+            case 5:
+                IQRString = "Z[phi]"; // phi = 1/2 + sqrt(5)/2, the golden ratio
+                break;
+            default:
+                if (this.d1mod4) {
+                    IQRString = "O_(Q(sqrt(" + this.radicand + ")))";
+                } else {
+                    IQRString = "Z[sqrt(" + this.radicand + ")]";
+                }
+        }
+        return IQRString;
+    }
 
     /**
-     * I have not tested this function.
-     * @return A string suitable for use in a TeX document, if I haven't made any mistakes.
+     * A text representation of the ring's label suitable for use in a TeX 
+     * document. The representation uses blackboard bold unless 
+     * <code>preferBlackboardBold(false)</code> is in effect.
+     * I have not tested this function in the context of outputting to a TeX 
+     * document.
+     * @return A String suitable for use in a TeX document, if I haven't made 
+     * any mistakes. Examples: assuming the default preference for blackboard 
+     * bold, for d = -7, the result is "\mathcal O_{\mathbb Q(\sqrt{-7})}"; for 
+     * d = -5, the result is "\mathbb Z[\sqrt{-5}]"; for d = -3, the result is 
+     * "\mathbb Z[\omega]"; for d = 2, the result is "\mathbb Z[\sqrt{2}]"; for 
+     * d = 5, the result is "\mathbb Z[\phi]".
      */
+    @Override
     public String toTeXString() {
         String IQRString;
         String QChar;
@@ -83,33 +150,143 @@ public class QuadraticRing implements IntegerRing {
             QChar = "\\textbf Q";
             ZChar = "\\textbf Z";
         }
-        if (this.d1mod4) {
-            IQRString = "\\mathcal O_{" + QChar + "(\\sqrt{" + this.radicand + "})}";
-        } else {
-            IQRString = ZChar + "[\\sqrt{" + this.radicand + "}]";
+        switch (this.radicand) {
+            case -1:
+                IQRString = ZChar + "[i]";
+                break;
+            case -3:
+                IQRString = ZChar + "[\\omega]";
+                break;
+            case 5:
+                IQRString = ZChar + "[\\phi]";
+                break;
+            default:
+                if (this.d1mod4) {
+                    IQRString = "\\mathcal O_{" + QChar + "(\\sqrt{" + this.radicand + "})}";
+                } else {
+                    IQRString = ZChar + "[\\sqrt{" + this.radicand + "}]";
+            }
         }
         return IQRString;
     }
     
+    /**
+     * A text representation of the ring's label suitable for use in an HTML 
+     * document. The representation uses blackboard bold unless 
+     * <code>preferBlackboardBold(false)</code> is in effect.
+     * I have not tested this function in the context of outputting to an HTML 
+     * document.
+     * @return A String suitable for use in an HTML document, if I haven't made 
+     * any mistakes. If preferenceForBlackboardBold is true, this also assumes 
+     * the font chosen by the browser has the relevant Unicode characters. 
+     * Examples: with preference for blackboard bold on, we should get 
+     * "<i>O</i><sub>\u211A(&radic;-7)</sub>" for d = -7; for d = -5, the result 
+     * is "\u2124[&radic;-5]"; for d = -3, the result is "\u2124[&omega;]"; for 
+     * d = 2, the result should be "\u2124[&radic;2]"; for d = 5, the result 
+     * should be "\u2124[&phi;]". If instead preference for blackboard bold is 
+     * off, we should get "<i>O</i><sub><b>Q</b>(&radic;-7)</sub>" for d = -7; 
+     * for d = -5, the result is "<b>Z</b>[&radic;-5]"; for d = -3, the result 
+     * is "<b>Z</b>[&omega;]", for d = 2, result is "<b>Z</b>[&radic;2]"; for d 
+     * = 5, the result is "<b>Z</b>[&phi;]".
+     */
+    @Override
     public String toHTMLString() {
         String IQRString;
         String QChar;
         String ZChar;
         if (preferenceForBlackboardBold) {
-            QChar = "\\mathbb Q";
-            ZChar = "\\mathbb Z";
+            QChar = "\u211A"; // Double-struck capital Q
+            ZChar = "\u2124"; // Double-struck capital Z
         } else {
             QChar = "<b>Q</b>";
             ZChar = "<b>Z</b>";
         }
-        if (this.d1mod4) {
-            IQRString = "\\mathcal O_{" + QChar + "(\\sqrt{" + this.radicand + "})}";
-        } else {
-            IQRString = ZChar + "[\\sqrt{" + this.radicand + "}]";
+        switch (this.radicand) {
+            case -3:
+                IQRString = ZChar + "[&omega;]";
+                break;
+            case -1:
+                IQRString = ZChar + "[<i>i</i>]";
+                break;
+            case 5:
+                IQRString = ZChar + "[&phi;]";
+                break;
+            default:
+                if (this.d1mod4) {
+                    IQRString = "<i>O</i><sub>" + QChar + "(&radic;(" + this.radicand + ")</sub>";
+                } else {
+                    IQRString = ZChar + "[&radic;" + this.radicand + "]";
+                }
         }
         return IQRString;
     }
     
+    /**
+     * A text representation of the ring's label suitable for use in a filename.
+     * With our modern operating systems, it may be unnecessary to worry about 
+     * illegal characters in {@link #toString()}. But just in case, here is a 
+     * function whose output will hopefully help the calling program conform to 
+     * the old MS-DOS 8.3 standard. There is one context in which I have 
+     * definitely found this function useful, and that is in the output of the 
+     * test suite, for which the font seems to lack Greek letters.
+     * @return A string suitable for use in a filename. Examples: for d = -1, 
+     * returns "ZI"; for d = -2, returns "ZI2"; for d = -3, returns "ZW" (a 
+     * horrible kludge); for d = -7, returns "OQI7".
+     */
+    public String toFilenameString() {
+        String IQRString;
+        int radNum = this.radicand;
+        switch (radNum) {
+            case -3:
+                IQRString = "ZW"; // It is frowned upon to use "w" as a makeshift omega, but for the purpose here it is acceptable.
+                break;
+            case -1:
+                IQRString = "ZI";
+                break;
+            case 5:
+                IQRString = "ZPhi";
+                break;
+            default:
+                if (this.d1mod4) {
+                    IQRString = "OQ";
+                } else {
+                    IQRString = "Z";
+                }
+                if (this.radicand < 0) {
+                    IQRString = IQRString + "I";
+                    radNum *= -1;
+                }
+                IQRString = IQRString + radNum;
+        }
+        return IQRString;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final QuadraticRing other = (QuadraticRing) obj;
+        return (this.radicand != other.radicand);
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = this.radicand;
+        if (this.d1mod4) {
+            hash--;
+        } else {
+            hash *= -1;
+        }
+        return hash;
+    }
+
     @Override
     public int getMaxAlgebraicDegree() {
         return MAX_ALGEBRAIC_DEGREE;
