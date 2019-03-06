71404c99bb643aaf2d7004c21441a99f6fc183e2
Alonso del Arte <alonso.delarte@gmail.com>
2019-03-05 19:18:51 -0500

    Add files via upload

--- code-source/algebraics/quadratics/RealQuadraticInteger.java
+++ code-source/algebraics/quadratics/RealQuadraticInteger.java
@@ -18,10 +18,12 @@ package algebraics.quadratics;
 
 /**
  * Defines objects to represent real quadratic integers, for the most part 
- * symbolically rather than numerically.
+ * symbolically rather than numerically. Is <code>Comparable</code>, enabling 
+ * sorting through <code>Collections.sort()</code> or <code>Arrays.sort()</code> 
+ * in ascending order according to position on the real number line.
  * @author Alonso del Arte
  */
-public class RealQuadraticInteger extends QuadraticInteger {
+public class RealQuadraticInteger extends QuadraticInteger implements Comparable<RealQuadraticInteger> {
     
     private final double numVal;
     private final double absNumVal;
@@ -50,8 +52,8 @@ public class RealQuadraticInteger extends QuadraticInteger {
     }
     
     /**
-     * Gets the imaginary part of the real quadratic integer. But for a real 
-     * quadratic integer, this should actually always be exactly 0.
+     * Gets the imaginary part of the real quadratic integer. This should always 
+     * be exactly 0.
      * @return The imaginary part of the real quadratic integer. For example, 
      * for &minus;1/2 + 9(&radic;13)/2, the result should be 0.0.
      */
@@ -79,6 +81,47 @@ public class RealQuadraticInteger extends QuadraticInteger {
     }
     
     /**
+     * Compares the number represented by this RealQuadraticInteger object with 
+     * the number represented by the specified RealQuadraticInteger object for 
+     * order. Returns a negative integer, zero, or a positive integer as this 
+     * real quadratic integer is less than, equal to, or greater than the 
+     * specified real quadratic integer. This enables sorting with {@link 
+     * java.util.Collections#sort(java.util.List)} without need for a 
+     * comparator. If you need to sort by norm, use {@link 
+     * calculators.NumberTheoreticFunctionsCalculator#sortListAlgebraicIntegersByNorm(java.util.List)}.
+     * @param other The real quadratic integer to compare this real quadratic 
+     * integer to. The comparison is more reliable if all involved real 
+     * quadratic integers come from the same ring. Examples: &minus;&radic;21 
+     * (roughly &minus;4.58257569), 4 + &radic;21 (roughly 8.58257569), 
+     * &minus;3/2 + (13&radic;21)/2 (roughly 28.286742).
+     * @return &minus;1 or any other negative integer if this real quadratic 
+     * integer is less than the other real quadratic integer; 0 if this and the 
+     * other real quadratic integers are equal; 1 or any positive integer if 
+     * this real quadratic integer is greater than the other real quadratic 
+     * integer. For example, if this real quadratic integer is 4 + &radic;21, 
+     * then, compared to &minus;&radic;21, the result will most likely be 1; 
+     * compared to 4 + &radic;21, the result must be 0; and compared to 
+     * &minus;3/2 + (13&radic;21)/2, the result will most likely be &minus;1.
+     */
+    @Override
+    public int compareTo(RealQuadraticInteger other) {
+        double diffRe;
+        if (this.quadRing.equals(other.quadRing)) {
+            QuadraticInteger diff = this.minus(other);
+            diffRe = diff.getRealPartNumeric();
+        } else {
+            diffRe = this.numVal - other.numVal;
+        }
+        if (diffRe < 0) {
+            return -1;
+        }
+        if (diffRe > 0) {
+            return 1;
+        }
+        return 0;
+    }
+    
+    /**
      * Alternative constructor, may be used when the denominator is known to be 
      * 1. For example, this constructor may be used for 1 + &radic;5. For 1/2 + 
      * (&radic;5)/2, it will be necessary to use the primary constructor. One 
@@ -90,12 +133,12 @@ public class RealQuadraticInteger extends QuadraticInteger {
      * @param b The part to be multiplied by &radic;<i>d</i>. For example, for 5 
      * + 4&radic;3, this parameter would be 4.
      * @param R The ring to which this algebraic integer belongs to. For 
-     * example, for 5 + &radic;3, this parameter could be <code>new 
+     * example, for 5 + 4&radic;3, this parameter could be <code>new 
      * RealQuadraticRing(3)</code>.
-     * @throws IllegalArgumentException If R is not of the type {@link 
-     * RealQuadraticRing}. However, if R is of type {@link 
-     * ImaginaryQuadraticRing} and b = 0, a real ring will be quietly 
-     * substituted.
+     * @throws IllegalArgumentException If <code>R</code> is not of the type 
+     * {@link RealQuadraticRing}. However, if <code>R</code> is of type {@link 
+     * ImaginaryQuadraticRing} and <code>b = 0</code>, a real ring will be 
+     * quietly substituted.
      */
     public RealQuadraticInteger(int a, int b, QuadraticRing R) {
         this(a, b, R, 1);
@@ -118,13 +161,14 @@ public class RealQuadraticInteger extends QuadraticInteger {
      * true. If that is the case, &minus;2 may also be used, and &minus;1 can 
      * always be used; the constructor will quietly substitute 1 or 2 and 
      * multiply a and b by &minus;1.
-     * @throws IllegalArgumentException If denom = 2 but there is a parity 
-     * mismatch between a and b (that is, one is odd and the other is even), or 
-     * if denom = 2 but R{@link QuadraticRing#hasHalfIntegers() 
-     * .hasHalfIntegers()} is false. This exception may also arise if R is not 
-     * of the type {@link RealQuadraticRing}. However, if R is of type {@link 
-     * ImaginaryQuadraticRing} and b = 0, a real ring will be quietly 
-     * substituted.
+     * @throws IllegalArgumentException If <code>denom</code> is 2 but there is 
+     * a parity mismatch between <code>a</code> and <code>b</code> (that is, one 
+     * is odd and the other is even), or if <code>denom</code> is 2 but 
+     * <code>R</code>{@link QuadraticRing#hasHalfIntegers() .hasHalfIntegers()} 
+     * is false. This exception may also arise if <code>R</code> is not of the 
+     * type {@link RealQuadraticRing}. However, if <code>R</code> is of type 
+     * {@link ImaginaryQuadraticRing} and <code>b</code> is 0, a real ring will 
+     * be quietly substituted.
      */
     public RealQuadraticInteger(int a, int b, QuadraticRing R, int denom) {
         RealQuadraticRing ring;
