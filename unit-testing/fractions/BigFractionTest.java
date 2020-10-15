58b3f22f95ad14e291e5823e77e5bb3bb1b5f2ce
Alonso del Arte <alonso.delarte@gmail.com>
2020-10-15 01:14:08 -0400

    Getter and comparable tests

--- unit-testing/fractions/BigFractionTest.java
+++ unit-testing/fractions/BigFractionTest.java
@@ -19,6 +19,8 @@ package fractions;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Random;
 
@@ -32,6 +34,26 @@ import static org.junit.Assert.*;
 public class BigFractionTest {
     
     private final BigInteger TWO = BigInteger.ONE.add(BigInteger.ONE);
+    
+    @Test
+    public void testGetNumerator() {
+        System.out.println("getNumerator");
+        BigInteger expected = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger denom = BigInteger.valueOf(Long.MIN_VALUE).negate();
+        BigFraction fraction = new BigFraction(expected, denom);
+        BigInteger actual = fraction.getNumerator();
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testGetDenominator() {
+        System.out.println("getDenominator");
+        BigInteger numer = BigInteger.valueOf(Long.MAX_VALUE);
+        BigInteger expected = BigInteger.valueOf(Long.MIN_VALUE).negate();
+        BigFraction fraction = new BigFraction(numer, expected);
+        BigInteger actual = fraction.getDenominator();
+        assertEquals(expected, actual);
+    }
     
     @Test
     public void testToString() {
@@ -92,6 +114,67 @@ public class BigFractionTest {
         BigFraction twoThirds = new BigFraction(TWO, three);
         BigFraction twoFifths = new BigFraction(TWO, five);
         assertNotEquals(twoThirds, twoFifths);
+    }
+    
+    @Test
+    public void testEqualsCompareToCorrespondence() {
+        BigInteger sixty = BigInteger.valueOf(60);
+        BigFraction fractionA, fractionB;
+        int comparisonAToB, comparisonBToA, signProd;
+        String msg;
+        for (BigInteger numer = BigInteger.ONE; numer.compareTo(sixty) < 0;
+                numer = numer.add(BigInteger.ONE)) {
+            for (BigInteger denom = BigInteger.ONE; denom.compareTo(sixty) < 0; 
+                    denom = denom.add(BigInteger.ONE)) {
+                fractionA = new BigFraction(numer, denom);
+                fractionB = new BigFraction(numer.add(TWO), denom.add(TWO));
+                comparisonAToB = fractionA.compareTo(fractionB);
+                comparisonBToA = fractionB.compareTo(fractionA);
+                msg = fractionA.toString() + " compareTo " + fractionB.toString();
+                if (fractionA.equals(fractionB)) {
+                    msg = msg + " should be 0, and vice-versa";
+                    assert comparisonAToB == 0 : msg;
+                    assert comparisonBToA == 0 : msg;
+                } else {
+                    msg = msg + " should have sign opposite " 
+                            + fractionB.toString() + " compareTo " 
+                            + fractionA.toString();
+                    signProd = Integer.signum(comparisonAToB) 
+                            * Integer.signum(comparisonBToA);
+                    assert signProd == -1 : msg;
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+        ArrayList<BigFraction> expected = new ArrayList<>();
+        BigInteger numer = BigInteger.ONE.negate();
+        BigFraction negativeOneHalf = new BigFraction(numer, TWO);
+        expected.add(negativeOneHalf);
+        BigFraction zero = new BigFraction(BigInteger.ZERO, TWO);
+        expected.add(zero);
+        numer = numer.add(TWO);
+        BigInteger denom = BigInteger.valueOf(Long.MAX_VALUE);
+        BigFraction smallFraction = new BigFraction(numer, denom);
+        expected.add(smallFraction);
+        numer = denom.subtract(TWO);
+        BigFraction nearOne = new BigFraction(numer, denom);
+        expected.add(nearOne);
+        numer = BigInteger.valueOf(355);
+        denom = BigInteger.valueOf(113);
+        BigFraction piApprox = new BigFraction(numer, denom);
+        expected.add(piApprox);
+        ArrayList<BigFraction> actual = new ArrayList<>();
+        actual.add(smallFraction);
+        actual.add(negativeOneHalf);
+        actual.add(nearOne);
+        actual.add(piApprox);
+        actual.add(zero);
+        Collections.sort(actual);
+        assertEquals(expected, actual);
     }
     
     @Test
@@ -276,9 +359,50 @@ public class BigFractionTest {
         assertEquals(expected, actual);
     }
     
-    // TODO: Write test for reciprocal
+    @Test
+    public void testReciprocal() {
+        System.out.println("reciprocal");
+        BigInteger three = TWO.add(BigInteger.ONE);
+        BigInteger seven = three.multiply(TWO).add(BigInteger.ONE);
+        BigFraction threeSevenths = new BigFraction(three, seven);
+        BigFraction expected = new BigFraction(seven, three);
+        BigFraction actual = threeSevenths.reciprocal();
+        assertEquals(expected, actual);
+    }
     
-    // TODO: Write tests for dividedBy
+    @Test
+    public void testDividedBy() {
+        System.out.println("dividedBy");
+        BigFraction one = new BigFraction(BigInteger.ONE, BigInteger.ONE);
+        BigInteger numer = BigInteger.valueOf(Long.MAX_VALUE);
+        BigFraction divisor = new BigFraction(numer, BigInteger.ONE);
+        BigFraction expected = new BigFraction(BigInteger.ONE, numer);
+        BigFraction actual = one.dividedBy(divisor);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testDividedBySmallFraction() {
+        BigInteger numer = TWO.pow(21);
+        BigInteger denom = TWO.pow(65).subtract(BigInteger.ONE);
+        BigFraction fraction = new BigFraction(numer, denom);
+        Fraction oneHalf = new Fraction(1, 2);
+        BigFraction expected = new BigFraction(numer.multiply(TWO), denom);
+        BigFraction actual = fraction.dividedBy(oneHalf);
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testDividedByInt() {
+        int divisor = 7;
+        BigInteger seven = BigInteger.valueOf(divisor);
+        BigInteger numer = seven.multiply(TWO).add(BigInteger.ONE);
+        BigInteger denom = seven.pow(23);
+        BigFraction fraction = new BigFraction(numer, denom);
+        BigFraction expected = new BigFraction(numer, denom.multiply(seven));
+        BigFraction actual = fraction.dividedBy(divisor);
+        assertEquals(expected, actual);
+    }
     
     @Test
     public void testGetDouble() {
