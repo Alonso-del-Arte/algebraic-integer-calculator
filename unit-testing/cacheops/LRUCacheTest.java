42e868ec93dd84c3f71b15011eb339984e109d21
Alonso del Arte <alonso.delarte@gmail.com>
2021-04-29 01:44:03 -0400

    New test class

---
+++ unit-testing/cacheops/LRUCacheTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2021 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cacheops;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class LRUCacheTest {
+    
+    /**
+     * Test of create method, of class LRUCache.
+     */
+    @Test
+    public void testCreate() {
+        System.out.println("create");
+//        Object name = null;
+//        LRUCache instance = null;
+//        Object expResult = null;
+//        Object result = instance.create(name);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of moveToFront method, of class LRUCache.
+     */
+    @Test
+    public void testMoveToFront() {
+        System.out.println("moveToFront");
+//        Object[] objects = null;
+//        int i = 0;
+//        LRUCache.moveToFront(objects, i);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of hasName method, of class LRUCache.
+     */
+    @Test
+    public void testHasName() {
+        System.out.println("hasName");
+//        Object value = null;
+//        Object name = null;
+//        LRUCache instance = null;
+//        boolean expResult = false;
+//        boolean result = instance.hasName(value, name);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of forName method, of class LRUCache.
+     */
+    @Test
+    public void testForName() {
+        System.out.println("forName");
+//        Object name = null;
+//        LRUCache instance = null;
+//        Object expResult = null;
+//        Object result = instance.forName(name);
+//        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
