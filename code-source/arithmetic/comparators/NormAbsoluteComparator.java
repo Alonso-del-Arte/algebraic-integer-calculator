afad4fc80d4d4a3337da7d036f8b8563c3e016cf
Alonso del Arte <alonso.delarte@gmail.com>
2021-05-09 17:55:51 -0400

    Package restructing plus passing tests

--- code-source/arithmetic/comparators/NormAbsoluteComparator.java
+++ code-source/arithmetic/comparators/NormAbsoluteComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Alonso del Arte
+ * Copyright (C) 2021 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -14,26 +14,48 @@
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package algebraics;
+package arithmetic.comparators;
+
+import algebraics.AlgebraicInteger;
 
 import java.util.Comparator;
 
 /**
  * A comparator for use in sorting procedures or functions from the JDK, to sort 
- * instances of {@link AlgebraicInteger} by norm.
- * An early version of {@link calculators.NumberTheoreticFunctionsCalculator} 
- * has a <code>sortListAlgebraicIntegersByNorm()</code> function that is 
- * deprecated as of Version 0.9 and will be removed before Version 1.0.
+ * instances of {@link AlgebraicInteger} by the absolute values of their norms. 
+ * Another comparator is necessary if you need to sort algebraic instances with 
+ * the same absolute value of norm by some other criterion.
+ * <p>An early version of {@link calculators.NumberTheoreticFunctionsCalculator} 
+ * had a <code>sortListAlgebraicIntegersByNorm()</code> function that was 
+ * deprecated in Version 0.9 and removed completely before Version 1.0.</p>
  * @since Version 0.9
  * @author Alonso del Arte
  */
 public class NormAbsoluteComparator implements Comparator<AlgebraicInteger> {
     
-    // TODO: Write tests for this
-    // TODO: Write Javadoc
+    /**
+     * Compares two algebraic integers according to the absolute values of their 
+     * norms. The two algebraic integers ought to be from the same ring, but 
+     * this function does not check that the rings match.
+     * @param numberA The first number to compare. For example, 2 + &radic;7, 
+     * which has norm &minus;3.
+     * @param numberB The second number to compare. Three examples: 3 + 
+     * &radic;7, which has norm 2; 2 + &radic;7, which has norm &minus;3; and 
+     * &minus;7 + 2&radic;7, which has norm 21.
+     * @return A negative integer if <code>numberA</code> has a norm with lesser 
+     * absolute value than <code>numberB</code>, 0 if <code>numberA</code> and 
+     * <code>numberB</code> have the same absolute value of norm, and a positive 
+     * integer if <code>numberA</code> has a norm with greater absolute value 
+     * than <code>numberB</code>. For example, given <code>numberA</code> being 
+     * 2 + &radic;7, if <code>numberB</code> is 3 + &radic;7, this function will 
+     * return 1 or greater (because, although &minus;3 &lt; 2, this goes by 
+     * absolute value, so we have that 3 &gt; 2); if <code>numberB</code> is 2 +
+     * &radic;7, this function will return 0; and if <code>numberB</code> is 
+     * &minus;7 + 2&radic;7, this function will return &minus;1 or less.
+     */
     @Override
     public int compare(AlgebraicInteger numberA, AlgebraicInteger numberB) {
-        return 0;
+        return Long.compare(Math.abs(numberA.norm()), Math.abs(numberB.norm()));
     }
     
 }
--- code-source/arithmetic/comparators/NormComparator.java
+++ code-source/arithmetic/comparators/NormComparator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Alonso del Arte
+ * Copyright (C) 2021 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -14,26 +14,46 @@
  * You should have received a copy of the GNU General Public License along with 
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package algebraics;
+package arithmetic.comparators;
+
+import algebraics.AlgebraicInteger;
 
 import java.util.Comparator;
 
 /**
  * A comparator for use in sorting procedures or functions from the JDK, to sort 
- * instances of {@link AlgebraicInteger} by norm.
- * An early version of {@link calculators.NumberTheoreticFunctionsCalculator} 
- * has a <code>sortListAlgebraicIntegersByNorm()</code> function that is 
- * deprecated as of Version 0.9 and will be removed before Version 1.0.
+ * instances of {@link AlgebraicInteger} by norm. Another comparator is 
+ * necessary if you need to sort algebraic instances with the same norm by some 
+ * other criterion.
+ * <p>An early version of {@link calculators.NumberTheoreticFunctionsCalculator} 
+ * had a <code>sortListAlgebraicIntegersByNorm()</code> function that was 
+ * deprecated in Version 0.9 and removed completely before Version 1.0.</p>
  * @since Version 0.9
  * @author Alonso del Arte
  */
 public class NormComparator implements Comparator<AlgebraicInteger> {
     
-    // TODO: Write tests for this
-    // TODO: Write Javadoc
+    /**
+     * Compares two algebraic integers according to norm. The two algebraic 
+     * integers ought to be from the same ring, but this function does not check 
+     * that the rings match.
+     * @param numberA The first number to compare. For example, 3 + &radic;7, 
+     * which has norm 2.
+     * @param numberB The second number to compare. Three examples: 2 + 
+     * &radic;7, which has norm &minus;3; 3 + &radic;7, which has norm 2; and 
+     * &minus;7 + 2&radic;7, which has norm 21.
+     * @return A negative integer if <code>numberA</code> has lesser norm than 
+     * <code>numberB</code>, 0 if <code>numberA</code> and <code>numberB</code> 
+     * have the same norm, and a positive integer if <code>numberA</code> has 
+     * greater norm than <code>numberB</code>. For example, given 
+     * <code>numberA</code> being 3 + &radic;7, if <code>numberB</code> is 2 + 
+     * &radic;7, this function will return 1 or greater; if <code>numberB</code> 
+     * is 3 + &radic;7, this function will return 0; and if <code>numberB</code> 
+     * is &minus;7 + 2&radic;7, this function will return &minus;1 or less.
+     */
     @Override
     public int compare(AlgebraicInteger numberA, AlgebraicInteger numberB) {
-        return 0;
+        return Long.compare(numberA.norm(), numberB.norm());
     }
     
 }
