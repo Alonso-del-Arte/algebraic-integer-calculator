3d2725f4af879d3e8582c5e05e0b7e8d6dd57a06
Alonso del Arte <alonso.delarte@gmail.com>
2021-12-01 20:18:52 -0500

    Groundwork for constructor

--- code-source/calculators/AlgebraicIntegerCalculator.java
+++ code-source/calculators/AlgebraicIntegerCalculator.java
@@ -16,17 +16,64 @@
  */
 package calculators;
 
+import algebraics.IntegerRing;
+import algebraics.quadratics.ImaginaryQuadraticInteger;
 import algebraics.quadratics.ImaginaryQuadraticRing;
+import algebraics.quadratics.QuadraticInteger;
+import algebraics.quadratics.QuadraticRing;
+import algebraics.quadratics.RealQuadraticInteger;
 import algebraics.quadratics.RealQuadraticRing;
 import viewers.ImagQuadRingDisplay;
 import viewers.RealQuadRingDisplay;
-import viewers.Zeta8RingDisplay;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.Event;
+import java.awt.Graphics2D;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileFilter;
 
 /**
  * This will eventually be the main class of the project.
  * @author Alonso del Arte
  */
 public class AlgebraicIntegerCalculator {
+    
+    private JFrame frame;
+    
+    public AlgebraicIntegerCalculator(IntegerRing ring) {
+        if (ring == null) {
+            String excMsg = "Ring should not be null";
+        }
+//        if (!(ring instanceof QuadraticRing)) {
+//            String excMsg = "Sorry, rings of type " + ring.getClass().getName() 
+//                    + " are not yet supported";
+//        }
+    }
 
     /**
      * at first this will just the of the
@@ -37,15 +84,6 @@ public class AlgebraicIntegerCalculator {
             System.setProperty("apple.laf.useScreenMenuBar", "true");
         }
         System.out.println("Still working on the main program...");
-        System.out.println("In the meantime, please enjoy the ring viewers...");
-        ImaginaryQuadraticRing imagQuadRing = new ImaginaryQuadraticRing(-1);
-        ImagQuadRingDisplay iqrd = new ImagQuadRingDisplay(imagQuadRing);
-        iqrd.startRingDisplay();
-        RealQuadraticRing realQuadRing = new RealQuadraticRing(2);
-        RealQuadRingDisplay rqrd = new RealQuadRingDisplay(realQuadRing);
-        rqrd.startRingDisplay();
-        Zeta8RingDisplay zrd = new Zeta8RingDisplay();
-        zrd.startRingDisplay();
     }
     
 }
