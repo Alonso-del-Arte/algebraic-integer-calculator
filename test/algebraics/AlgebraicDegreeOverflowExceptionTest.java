ec8bcecba690d5fdddc0d7384163490a45350b1f
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-03 18:24:17 -0400

    Failing getMaxExpectedAlgebraicDegree( ) test

--- test/algebraics/AlgebraicDegreeOverflowExceptionTest.java
+++ test/algebraics/AlgebraicDegreeOverflowExceptionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Alonso del Arte
+ * Copyright (C) 2023 Alonso del Arte
  *
  * This program is free software: you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
@@ -16,6 +16,8 @@
  */
 package algebraics;
 
+import algebraics.quadratics.IllDefinedQuadraticInteger;
+import algebraics.quadratics.IllDefinedQuadraticRing;
 import algebraics.quadratics.ImaginaryQuadraticInteger;
 import algebraics.quadratics.ImaginaryQuadraticRing;
 import algebraics.quadratics.QuadraticInteger;
@@ -23,6 +25,7 @@ import algebraics.quadratics.QuadraticRing;
 import algebraics.quadratics.RealQuadraticInteger;
 import algebraics.quadratics.RealQuadraticRing;
 import calculators.NumberTheoreticFunctionsCalculator;
+import static calculators.NumberTheoreticFunctionsCalculator.randomNumber;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -34,97 +37,61 @@ import static org.junit.Assert.*;
  */
 public class AlgebraicDegreeOverflowExceptionTest {
     
-    private static AlgebraicDegreeOverflowException algDegOvflExcImag;
-    private static AlgebraicDegreeOverflowException algDegOvflExcReal;
+    private static final String DEFAULT_MESSAGE = "FOR TESTING PURPOSES ONLY";
     
     /**
-     * Sets up the AlgebraicDegreeOverflowException objects to use in the tests. 
-     * If any of these objects fail to initialize properly, it will have the 
-     * exception detail message "Initialization state, not the result of an 
-     * actually thrown exception."
-     */
-    @BeforeClass
-    public static void setUpClass() {
-        algDegOvflExcImag = new AlgebraicDegreeOverflowException("Initialization state, not the result of an actually thrown exception.", 2, NumberTheoreticFunctionsCalculator.IMAG_UNIT_I, NumberTheoreticFunctionsCalculator.IMAG_UNIT_NEG_I);
-        algDegOvflExcReal = new AlgebraicDegreeOverflowException("Initialization state, not the result of an actually thrown exception.", 2, NumberTheoreticFunctionsCalculator.IMAG_UNIT_I, NumberTheoreticFunctionsCalculator.IMAG_UNIT_NEG_I);
-        QuadraticRing ring = new ImaginaryQuadraticRing(-2);
-        QuadraticInteger numberA = new ImaginaryQuadraticInteger(14, 7, ring);
-        ring = new ImaginaryQuadraticRing(-7);
-        QuadraticInteger numberB = new ImaginaryQuadraticInteger(7, 1, ring);
-        QuadraticInteger sum;
-        try {
-            sum = numberA.plus(numberB);
-            System.out.println(numberA.toASCIIString() + " + " + numberB.toASCIIString() + " = " + sum.toASCIIString());
-        } catch (AlgebraicDegreeOverflowException adoe) {
-            algDegOvflExcImag = adoe;
-        } catch (Exception e) {
-            System.out.println("Exception " + e.getClass().getName() + " encountered.");
-            System.out.println("\"" + e.getMessage() + "\"");
-            System.out.println("Probably all the tests will fail now.");
-        }
-        System.out.println("AlgebraicDegreeOverflowException for the imaginary quadratic example has this message: \"" + algDegOvflExcImag.getMessage() + "\"");
-        ring = new RealQuadraticRing(3);
-        numberA = new RealQuadraticInteger(14, 7, ring);
-        ring = new RealQuadraticRing(10);
-        numberB = new RealQuadraticInteger(7, 1, ring);
-        try {
-            sum = numberA.plus(numberB);
-            System.out.println(numberA.toASCIIString() + " + " + numberB.toASCIIString() + " = " + sum.toASCIIString());
-        } catch (AlgebraicDegreeOverflowException adoe) {
-            algDegOvflExcReal = adoe;
-        } catch (Exception e) {
-            System.out.println("Exception " + e.getClass().getName() + " encountered.");
-            System.out.println("\"" + e.getMessage() + "\"");
-            System.out.println("Probably all the tests will fail now.");
-        }
-        System.out.println("AlgebraicDegreeOverflowException for the real quadratic example has this message: \"" + algDegOvflExcReal.getMessage() + "\"");
-    }
-    
-    /**
-     * Test of getMaxExpectedAlgebraicDegree method, of class 
-     * AlgebraicDegreeOverflowException.
+     * Test of the getMaxExpectedAlgebraicDegree function, of the 
+     * AlgebraicDegreeOverflowException class.
      */
     @Test
     public void testGetMaxExpectedAlgebraicDegree() {
         System.out.println("getMaxExpectedAlgebraicDegree");
-        int expAlgDeg = 2;
-        assertEquals(expAlgDeg, algDegOvflExcImag.getMaxExpectedAlgebraicDegree());
-        assertEquals(expAlgDeg, algDegOvflExcReal.getMaxExpectedAlgebraicDegree());
+        int expected = randomNumber(128) + 2;
+        BadRing ring = new BadRing(expected);
+        AlgebraicInteger numberA = new BadInteger(ring);
+        AlgebraicInteger numberB = new BadInteger(ring);
+        AlgebraicDegreeOverflowException exc 
+                = new AlgebraicDegreeOverflowException(DEFAULT_MESSAGE, 
+                        expected, numberA, numberB);
+        int actual = exc.getMaxExpectedAlgebraicDegree();
+        assertEquals(expected, actual);
     }
 
     /**
      * Test of getNecessaryAlgebraicDegree method, of class 
      * AlgebraicDegreeOverflowException.
-     */
+     */@org.junit.Ignore
     @Test
     public void testGetNecessaryAlgebraicDegree() {
         System.out.println("getNecessaryAlgebraicDegree");
-        int expAlgDeg = 4;
-        assertEquals(expAlgDeg, algDegOvflExcImag.getNecessaryAlgebraicDegree());
-        assertEquals(expAlgDeg, algDegOvflExcReal.getNecessaryAlgebraicDegree());
+        fail("REWRITE THIS TEST");
+//        int expAlgDeg = 4;
+//        assertEquals(expAlgDeg, algDegOvflExcImag.getNecessaryAlgebraicDegree());
+//        assertEquals(expAlgDeg, algDegOvflExcReal.getNecessaryAlgebraicDegree());
     }
 
     /**
      * Test of getCausingNumbers method, of class 
      * AlgebraicDegreeOverflowException.
-     */
+     */@org.junit.Ignore
     @Test
     public void testGetCausingNumbers() {
         System.out.println("getCausingNumbers");
-        QuadraticRing ring = new ImaginaryQuadraticRing(-2);
-        QuadraticInteger numberA = new ImaginaryQuadraticInteger(14, 7, ring);
-        ring = new ImaginaryQuadraticRing(-7);
-        QuadraticInteger numberB = new ImaginaryQuadraticInteger(7, 1, ring);
-        AlgebraicInteger[] expResult = new AlgebraicInteger[]{numberA, numberB};
-        AlgebraicInteger[] result = algDegOvflExcImag.getCausingNumbers();
-        assertArrayEquals(expResult, result);
-        ring = new RealQuadraticRing(3);
-        numberA = new RealQuadraticInteger(14, 7, ring);
-        ring = new RealQuadraticRing(10);
-        numberB = new RealQuadraticInteger(7, 1, ring);
-        expResult = new AlgebraicInteger[]{numberA, numberB};
-        result = algDegOvflExcReal.getCausingNumbers();
-        assertArrayEquals(expResult, result);
+        fail("REWRITE THIS TEST");
+//        QuadraticRing ring = new ImaginaryQuadraticRing(-2);
+//        QuadraticInteger numberA = new ImaginaryQuadraticInteger(14, 7, ring);
+//        ring = new ImaginaryQuadraticRing(-7);
+//        QuadraticInteger numberB = new ImaginaryQuadraticInteger(7, 1, ring);
+//        AlgebraicInteger[] expResult = new AlgebraicInteger[]{numberA, numberB};
+//        AlgebraicInteger[] result = algDegOvflExcImag.getCausingNumbers();
+//        assertArrayEquals(expResult, result);
+//        ring = new RealQuadraticRing(3);
+//        numberA = new RealQuadraticInteger(14, 7, ring);
+//        ring = new RealQuadraticRing(10);
+//        numberB = new RealQuadraticInteger(7, 1, ring);
+//        expResult = new AlgebraicInteger[]{numberA, numberB};
+//        result = algDegOvflExcReal.getCausingNumbers();
+//        assertArrayEquals(expResult, result);
     }
     
 }
