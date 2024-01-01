538da36d8ff5d8df31c9dbe68439f34e33d62565
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-01 02:08:18 -0500

    Stubs for IntegerRing functions

--- src/algebraics/unary/UnaryRing.java
+++ src/algebraics/unary/UnaryRing.java
@@ -1,13 +1,78 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package algebraics.unary;
 
+import algebraics.IntegerRing;
+import arithmetic.PowerBasis;
+import fractions.Fraction;
+
 /**
- *
- * @author al
+ * Represents the ring of integers of degree 1. Essentially this is another 
+ * wrapper for <code>int</code> primitives.
+ * @author Alonso del Arte
  */
-public class UnaryRing {
+class UnaryRing implements IntegerRing {
+    
+    public static final UnaryRing Z = new UnaryRing();
+    
+    private static final Fraction[] DRAFT_POWER_BASIS_FRACTIONS 
+            = {new Fraction(1, 2), new Fraction(1, 3)};
+
+    @Override
+    public int getMaxAlgebraicDegree() {
+        return Integer.MIN_VALUE;
+    }
+
+    @Override
+    public boolean isPurelyReal() {
+        return false;
+    }
+
+    @Override
+    public int discriminant() {
+        return 0;
+    }
+
+    @Override
+    public PowerBasis getPowerBasis() {
+        return new PowerBasis(DRAFT_POWER_BASIS_FRACTIONS);
+    }
+
+    @Override
+    public String toASCIIString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    @Override
+    public String toTeXString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    @Override
+    public String toHTMLString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    @Override
+    public String toFilenameString() {
+        return "NOT_IMPL.YET";
+    }
+    
+    private UnaryRing() {
+    }
     
 }
