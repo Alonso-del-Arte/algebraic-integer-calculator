2437f7e3bdb83604515df37551d41c86fc8b40ef
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-01 01:58:09 -0500

    New package

---
+++ test/algebraics/unary/UnaryRingTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package algebraics.unary;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author al
+ */
+public class UnaryRingTest {
+    
+    public UnaryRingTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
