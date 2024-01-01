1ddbca1f7ab04d3e83264f03c54ffd8efac83eb7
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-01 02:10:51 -0500

    Failing getMaxAlgebraicDegree( ) test

--- test/algebraics/unary/UnaryRingTest.java
+++ test/algebraics/unary/UnaryRingTest.java
@@ -1,6 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package algebraics.unary;
 
@@ -8,18 +20,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author al
+ * Tests of the UnaryRing class.
+ * @author Alonso del Arte
  */
 public class UnaryRingTest {
     
-    public UnaryRingTest() {
-    }
-
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetMaxAlgebraicDegree() {
+        System.out.println("getMaxAlgebraicDegree");
+        assertEquals(1, UnaryRing.Z.getMaxAlgebraicDegree());
     }
     
 }
