612a79f64ec78bd744d1c2e8d086495f5b494305
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-02 12:10:00 -0500

    Failing toTeXStringBlackboardBold( ) test

--- test/algebraics/IntegerRingTest.java
+++ test/algebraics/IntegerRingTest.java
@@ -1,22 +1,51 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package algebraics;
 
 import arithmetic.PowerBasis;
 import fractions.Fraction;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
- *
- * @author al
+ * Tests of the IntegerRing interface.
+ * @author Alonso del Arte
  */
 public class IntegerRingTest {
     
+    /**
+     * Test of the toTeXStringBlackboardBold function of the IntegerRing 
+     * interface. The default implementation should give the same result as 
+     * {@link IntegerRing#toTeXString()}.
+     */
+    @Test
+    public void testToTeXStringBlackboardBold() {
+        System.out.println("toTeXStringBlackboardBold");
+        IntegerRing ring = new MockRing();
+        String expected = ring.toTeXString();
+        String actual = ring.toTeXStringBlackboardBold();
+        assertEquals(expected, actual);
+    }
+    
     private static class MockRing implements IntegerRing {
 
-        private static final Fraction[] DRAFT_POWER_BASIS_FRACTIONS 
-                = {new Fraction(1, 2), new Fraction(1, 3)};
+        private static final Fraction[] FAKE_POWER_BASIS_FRACTIONS 
+                = {new Fraction(22, 7), new Fraction(355, 113)};
 
         @Override
         public int getMaxAlgebraicDegree() {
@@ -35,7 +64,7 @@ public class IntegerRingTest {
 
         @Override
         public PowerBasis getPowerBasis() {
-            return new PowerBasis(DRAFT_POWER_BASIS_FRACTIONS);
+            return new PowerBasis(FAKE_POWER_BASIS_FRACTIONS);
         }
 
         @Override
