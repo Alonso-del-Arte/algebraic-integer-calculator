5b2d4130a9cbcbc0bbec7d1fb024d4f5f73095ef
Alonso del Arte <alonso.delarte@gmail.com>
2024-01-10 23:40:11 -0500

    Failing toString( ) test

--- test/algebraics/unary/UnaryIntegerTest.java
+++ test/algebraics/unary/UnaryIntegerTest.java
@@ -1,25 +1,40 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package algebraics.unary;
+
+import static calculators.NumberTheoreticFunctionsCalculator.randomNumber;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author al
+ * Tests of the UnaryInteger class.
+ * @author Alonso del Arte
  */
 public class UnaryIntegerTest {
     
-    public UnaryIntegerTest() {
-    }
-
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testToString() {
+        System.out.println("toString");
+        int n = randomNumber(Short.MAX_VALUE);
+        UnaryInteger number = new UnaryInteger(n);
+        String expected = Integer.toString(n);
+        String actual = number.toString();
+        assertEquals(expected, actual);
     }
     
 }
