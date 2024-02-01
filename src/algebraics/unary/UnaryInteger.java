a55fb7dbcd73c7a6981835d0389c38d1770a28a3
Alonso del Arte <alonso.delarte@gmail.com>
2024-02-01 15:31:10 -0500

    Passing referential equality test

--- src/algebraics/unary/UnaryInteger.java
+++ src/algebraics/unary/UnaryInteger.java
@@ -199,11 +199,6 @@ public class UnaryInteger implements AlgebraicInteger,
     }
     
     @Override
-    public boolean equals(Object obj) {
-        return false;
-    }
-    
-    @Override
     public int hashCode() {
         return Integer.MIN_VALUE;
     }
