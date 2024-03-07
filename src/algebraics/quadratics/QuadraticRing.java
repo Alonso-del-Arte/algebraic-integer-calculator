3826418db793ed1b6a5815bbaf48fc4715a44003
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-06 22:11:45 -0500

    Passing equals( ) test

--- src/algebraics/quadratics/QuadraticRing.java
+++ src/algebraics/quadratics/QuadraticRing.java
@@ -371,18 +371,16 @@ public abstract class QuadraticRing implements IntegerRing, Serializable {
      * otherwise.
      */
     @Override
-    public boolean equals(Object obj) {return this == obj;
-//        if (this == obj) {
-//            return true;
-//        }
-//        if (obj == null) {
-//            return false;
-//        }
-//        if (this.getClass() != obj.getClass()) {
-//            return false;
-//        }
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
 //        final QuadraticRing other = (QuadraticRing) obj;
 //        return (this.radicand == other.radicand);
+        return this.getClass() == obj.getClass();
     }
     
     /**
