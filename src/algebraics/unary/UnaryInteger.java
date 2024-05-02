cddb2bf986084a70430def3cfe5408771bbc537c
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-02 00:06:41 -0400

    Passing minus int too negative test

--- src/algebraics/unary/UnaryInteger.java
+++ src/algebraics/unary/UnaryInteger.java
@@ -78,7 +78,8 @@ public final class UnaryInteger implements AlgebraicInteger,
     // TODO: Write tests for this
     @Override
     public UnaryInteger minus(int subtrahend) {
-        return new UnaryInteger(this.number - subtrahend);
+        int n = Math.subtractExact(this.number, subtrahend);
+        return new UnaryInteger(n);
     }
 
     // TODO: Write tests for this
