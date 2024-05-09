04b0d2ee7792b215b46079a4ccec54396e9436ae
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-08 22:52:44 -0400

    New class

---
+++ src/algebraics/cubics/PureCubicRing.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package algebraics.cubics;
+
+import arithmetic.PowerBasis;
+import fractions.Fraction;
+
+/**
+ * Represents the ring of integers of a pure cubic ring.
+ * @author Alonso del Arte
+ */
+public class PureCubicRing extends CubicRing {
+
+    // TODO: Write tests for this
+    @Override
+    public boolean isPurelyReal() {
+        return false;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public int discriminant() {
+        return 0;
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public PowerBasis getPowerBasis() {
+        Fraction fraction = new Fraction(1, 3);
+        Fraction[] array = {fraction};
+        return new PowerBasis(array);
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public String toASCIIString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public String toTeXString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public String toHTMLString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+
+    // TODO: Write tests for this
+    @Override
+    public String toFilenameString() {
+        return "NOT_IMPL.YET";
+    }
+    
+}
