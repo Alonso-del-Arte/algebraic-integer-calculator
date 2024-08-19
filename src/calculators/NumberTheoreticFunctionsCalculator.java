b4cdc50e798a7b1f45ffb97bb47ef7aaad1c36d9
Alonso del Arte <alonso.delarte@gmail.com>
2024-08-19 19:41:24 -0400

    Passing primeFactors( ) test, and back to passing almost all non-ignored tests

--- src/calculators/NumberTheoreticFunctionsCalculator.java
+++ src/calculators/NumberTheoreticFunctionsCalculator.java
@@ -277,27 +277,26 @@ public class NumberTheoreticFunctionsCalculator {
      * @since Version 0.1
      */
     public static List<Integer> primeFactors(int num) {
-//        int n = num;
+        int n = Math.abs(num);
         List<Integer> factors = new ArrayList<>();
-//        if (n == 0) {
-//            factors.add(0);
-//        } else {
+        if (n == 0 || n == 1) {
             factors.add(num);
+        } else {
 //            if (n < 0) {
 //                n *= (-1);
 //                factors.add(-1);
 //            }
-//            while (n % 2 == 0) {
-//                factors.add(2); // Treating 2 as a special case
-//                n /= 2;
-//            }
-//            for (int i = 3; i <= n; i += 2) {
-//                while (n % i == 0) {
-//                    factors.add(i);
-//                    n /= i;
-//                }
-//            }
-//        }
+            while (n % 2 == 0) {
+                factors.add(2);
+                n /= 2;
+            }
+            for (int i = 3; i <= n; i += 2) {
+                while (n % i == 0) {
+                    factors.add(i);
+                    n /= i;
+                }
+            }
+        }
         return factors;
     }
     
