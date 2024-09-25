5f4dc5070c2f0a40d29114b0a523809c1d0ceda3
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-24 23:46:45 -0400

    Passing rewritten apply( ) real test

--- src/algebraics/quadratics/QuadraticRing.java
+++ src/algebraics/quadratics/QuadraticRing.java
@@ -418,19 +418,19 @@ public abstract class QuadraticRing implements IntegerRing, Serializable {
     
     // TODO: Write tests for this
     public static QuadraticRing apply(int d) {
-//        String excMsg;
-//        switch (Integer.signum(d)) {
-//            case -1:
-                return new ImaginaryQuadraticRing(-Math.abs(d));
-//            case 0:
-//                excMsg = "0 is not valid for parameter d";
-//                throw new IllegalArgumentException(excMsg);
-//            case 1:
-//                return new RealQuadraticRing(Math.abs(d));
-//            default:
-//                excMsg = "Unexpected error for d = " + d;
-//                throw new RuntimeException(excMsg);
-//        }
+        String excMsg;
+        switch (Integer.signum(d)) {
+            case -1:
+                return new ImaginaryQuadraticRing(d);
+            case 0:
+                excMsg = "0 is not valid for parameter d";
+                throw new IllegalArgumentException(excMsg);
+            case 1:
+                return new RealQuadraticRing(Math.abs(d));
+            default:
+                excMsg = "Unexpected error for d = " + d;
+                throw new RuntimeException(excMsg);
+        }
     }
     
     /**
