fd8d4b799e5feb4528dc41325bbb98ec96ab75fb
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-18 22:41:42 -0400

    Refactored getRealPartNumeric( )

--- src/algebraics/quadratics/RealQuadraticInteger.java
+++ src/algebraics/quadratics/RealQuadraticInteger.java
@@ -52,8 +52,7 @@ public class RealQuadraticInteger extends QuadraticInteger
      */
     @Override
     public double getRealPartNumeric() {
-        return this.quadRing.getAbsNegRadSqrt() * this.surdPartMult 
-                + this.regPartMult;
+        return this.quadRing.realRadSqrt * this.surdPartMult + this.regPartMult;
     }
     
     /**
