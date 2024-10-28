6c296cce50bdebc29cbe283b6e0fb775f7f77e2b
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-27 23:43:44 -0400

    Stubs

--- src/algebraics/cubics/PureCubicInteger.java
+++ src/algebraics/cubics/PureCubicInteger.java
@@ -22,23 +22,126 @@ import fractions.Fraction;
  * Represents an algebraic integer in a pure cubic ring.
  * @author Alonso del Arte
  */
-public class PureCubicInteger {
+public class PureCubicInteger extends CubicInteger {
     
     private final PureCubicRing domain;
     
-    // TODO: Write Javadoc
+    // TODO: Write tests for this
+    @Override
+    public int algebraicDegree() {
+        return -1;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long trace() {
+        return 0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long norm() {
+        return 0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long[] minPolynomialCoeffs() {
+        long[] array = {0, 0, 0};
+        return array;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialStringTeX() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialStringHTML() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    /**
+     * Retrieves an object representing the ring this cubic integer belongs to.
+     * @return A {@link PureCubicRing} instance.
+     */
+    @Override
     public PureCubicRing getRing() {
         return this.domain;
     }
     
     // TODO: Write tests for this
+    @Override
+    public String toASCIIString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String toTeXString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String toHTMLString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double abs() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double getRealPartNumeric() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double getImagPartNumeric() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public boolean isReApprox() {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public boolean isImApprox() {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double angle() {
+        return 0.0;
+    }
+        
+    // TODO: Write tests for this
     public PureCubicInteger(int a, int b, int c, PureCubicRing ring) {
+        super(ring);
         this.domain = ring;
     }
     
     // TODO: Write tests for this
     public PureCubicInteger(Fraction a, Fraction b, Fraction c, 
             PureCubicRing ring) {
+        super(ring);
         this.domain = ring;
     }
     
