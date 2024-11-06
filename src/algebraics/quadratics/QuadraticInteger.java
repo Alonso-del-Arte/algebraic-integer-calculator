c7fe15432d64b19a8070efb2dbc16a2e23f1b893
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-05 23:19:10 -0500

    Rewind minPolynomialCoeffs( ) to failing

--- src/algebraics/quadratics/QuadraticInteger.java
+++ src/algebraics/quadratics/QuadraticInteger.java
@@ -181,28 +181,28 @@ public abstract class QuadraticInteger implements AlgebraicInteger,
      */
     @Override
     public long[] minPolynomialCoeffs() {
-        long[] coeffs = {0, 0, 0};
-        switch (this.algebraicDegree()) {
-            case 0:
-                coeffs[1] = 1;
-                break;
-            case 1: 
-                coeffs[0] = -1 * this.regPartMult;
-                coeffs[1] = 1;
-                break;
-            case 2: 
-                coeffs[0] = this.norm();
-                coeffs[1] = -this.trace();
-                coeffs[2] = 1;
-                break;
-            default:
-                coeffs[2] = Long.MIN_VALUE;
+        long[] coeffs = {0L, 0L, 0L};
+//        switch (this.algebraicDegree()) {
+//            case 0:
+//                coeffs[1] = 1;
+//                break;
+//            case 1: 
+//                coeffs[0] = -1 * this.regPartMult;
+//                coeffs[1] = 1;
+//                break;
+//            case 2: 
+//                coeffs[0] = this.norm();
+//                coeffs[1] = -this.trace();
+//                coeffs[2] = 1;
+//                break;
+//            default:
+//                coeffs[2] = Long.MIN_VALUE;
 //                String excMsg = "Excessive degree " + this.algebraicDegree() 
 //                        + " occurred somehow";
 //                Exception exc = new AlgebraicDegreeOverflowException(excMsg, 2, 
 //                        this, this);
 //                throw new RuntimeException(exc);
-        }
+//        }
         return coeffs;
     }
     
