508e579319e1784c496074629ba37e5d4f049325
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-10 22:23:54 -0500

    Passing norm of surd part only test

--- src/algebraics/quadratics/QuadraticInteger.java
+++ src/algebraics/quadratics/QuadraticInteger.java
@@ -141,6 +141,10 @@ public abstract class QuadraticInteger implements AlgebraicInteger,
     // TODO: Write tests for this
     @Override
     public long norm() {
+        if (this.regPartMult == 0) {
+            return -((long) this.surdPartMult * this.surdPartMult 
+                    * this.quadRing.radicand);
+        }
         return (long) this.regPartMult * this.regPartMult;
     }
     
