107aa65446287d8d6b45c25cfc5d9c32320e3f5f
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-13 01:12:36 -0500

    Passing equals( ) test

--- src/algebraics/quadratics/QuadraticInteger.java
+++ src/algebraics/quadratics/QuadraticInteger.java
@@ -622,6 +622,17 @@ public abstract class QuadraticInteger implements AlgebraicInteger,
     }
     
     @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        return this.getClass().equals(obj.getClass());
+    }
+    
+    @Override
     public int hashCode() {
         return 0;
     }
