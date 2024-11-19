590c4b4be2c4f22e99f5d5335e3608e910487b5a
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-18 22:51:51 -0500

    Failing toString( ) test with negative Re

--- test/algebraics/quadratics/ImaginaryQuadraticIntegerTest.java
+++ test/algebraics/quadratics/ImaginaryQuadraticIntegerTest.java
@@ -277,12 +277,25 @@ public class ImaginaryQuadraticIntegerTest {
     public void testToString() {
         System.out.println("toString");
         int bound = 8192;
-        int a = RANDOM.nextInt(2, bound);
+        int a = RANDOM.nextInt(1, bound);
         int b = RANDOM.nextInt(2, bound);
         QuadraticRing ring = chooseRing();
         QuadraticInteger number = new ImaginaryQuadraticInteger(a, b, ring);
         String expected = a + "+" + b + SQRT_SYMBOL + '(' + MINUS_SIGN 
                 + Math.abs(ring.getRadicand()) + ')';
+        String actual = number.toString().replace(" ", "");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testToStringNegativeRe() {
+        int bound = 8192;
+        int a = RANDOM.nextInt(1, bound);
+        int b = RANDOM.nextInt(2, bound);
+        QuadraticRing ring = chooseRing();
+        QuadraticInteger number = new ImaginaryQuadraticInteger(-a, b, ring);
+        String expected = MINUS_SIGN + a + "+" + b + SQRT_SYMBOL + '(' 
+                + MINUS_SIGN + Math.abs(ring.getRadicand()) + ')';
         String actual = number.toString().replace(" ", "");
         assertEquals(expected, actual);
     }
