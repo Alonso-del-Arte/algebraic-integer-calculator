62931bc2395196953ff4674dcdbf09f9c815d518
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-21 17:26:39 -0500

    Passing rewritten getSurdPartMult( ) test

--- src/algebraics/quadratics/RealQuadraticInteger.java
+++ src/algebraics/quadratics/RealQuadraticInteger.java
@@ -39,7 +39,7 @@ public class RealQuadraticInteger extends QuadraticInteger
 // TODO: Write tests for this
     @Override
     public int getSurdPartMult() {
-        return Integer.MAX_VALUE;
+        return this.surdPartMult * this.denominator;
     }
     
     /**
