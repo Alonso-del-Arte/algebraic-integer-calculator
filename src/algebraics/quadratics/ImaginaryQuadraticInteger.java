56b761d154996281bab1975478d3e55a38055aed
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-22 21:57:14 -0500

    Passing abs(0) test, still failing abs( ) test for purely real positive

--- src/algebraics/quadratics/ImaginaryQuadraticInteger.java
+++ src/algebraics/quadratics/ImaginaryQuadraticInteger.java
@@ -143,7 +143,7 @@ public class ImaginaryQuadraticInteger extends QuadraticInteger {
      */
     @Override
     public double abs() {
-        return 0.5 + this.regPartMult;
+        return 0.0;
     }
 
     /**
