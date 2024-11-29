2c774a4d58c2c2fedeb42a82e2e8df277f8f50bc
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-29 15:01:31 -0500

    Rewrote constructor rejects null ring as two tests; failing both and others

--- test/algebraics/quadratics/ImaginaryQuadraticIntegerTest.java
+++ test/algebraics/quadratics/ImaginaryQuadraticIntegerTest.java
@@ -3389,32 +3389,44 @@ public class ImaginaryQuadraticIntegerTest {
     }
     
     @Test
-    public void testConstructorNullRing() {
-        int a = 43;
-        int b = -577;
-        try {
-            ImaginaryQuadraticInteger badQuadInt 
-                    = new ImaginaryQuadraticInteger(a, b, null, 2);
-            String msg = "Should not have been able to create " 
-                    + badQuadInt.toString() + " with null ring";
-            fail(msg);
-        } catch (NullPointerException npe) {
-            System.out.println("Trying to use null ring correctly caused NullPointerException");
-            String excMsg = npe.getMessage();
-            if (excMsg == null) {
-                fail("Exception message should not be null");
-            } else {
-                System.out.println("\"" + excMsg + "\"");
-                String expected = "ring";
-                String msg = "Exception message contains the word \"" + expected 
-                        + "\"";
-                assert excMsg.toLowerCase().contains(expected) : msg;
-            }
-        } catch (RuntimeException re) {
-            String msg = re.getClass().getName() 
-                    + " is the wrong exception to throw for trying to use null ring";
-            fail(msg);
-        }
+    public void testConstructorRejectsNullRing() {
+        int a = randomNumber();
+        int b = randomNumber();
+        int denom = RANDOM.nextBoolean() ? 2 : 1;
+        String msg = "Should not be able to create instance with a = " + a 
+                + ", b = " + b + ", denom = " + denom + " and null ring";
+        Throwable t = assertThrows(() -> {
+            QuadraticInteger badInstance = new ImaginaryQuadraticInteger(a, b, 
+                    null, denom);
+            System.out.println(msg + ", but did create " 
+                    + badInstance.getClass().getName() + '@' 
+                    + Integer.toHexString(System
+                            .identityHashCode(badInstance)));
+        }, NullPointerException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        assert !excMsg.isBlank() : "Message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+    
+    @Test
+    public void testConstructorDefaultDenomRejectsNullRing() {
+        int a = randomNumber();
+        int b = randomNumber();
+        String msg = "Should not be able to create instance with a = " + a 
+                + ", b = " + b + " and null ring";
+        Throwable t = assertThrows(() -> {
+            QuadraticInteger badInstance = new ImaginaryQuadraticInteger(a, b, 
+                    null);
+            System.out.println(msg + ", but did create " 
+                    + badInstance.getClass().getName() + '@' 
+                    + Integer.toHexString(System
+                            .identityHashCode(badInstance)));
+        }, NullPointerException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Message should not be null";
+        assert !excMsg.isBlank() : "Message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
     }
     
 }
