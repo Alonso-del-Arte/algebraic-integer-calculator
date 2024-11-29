6a01e88e17408bbf755343e54ef8be842fa3f120
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-29 15:03:23 -0500

    Passing some constructor tests but failing other tests

--- src/algebraics/quadratics/ImaginaryQuadraticInteger.java
+++ src/algebraics/quadratics/ImaginaryQuadraticInteger.java
@@ -471,6 +471,10 @@ public class ImaginaryQuadraticInteger extends QuadraticInteger {
      */
     public ImaginaryQuadraticInteger(int a, int b, QuadraticRing ring, int denom) {
         super(a, b, ring, denom);
+        if (ring == null) {
+            String excMsg = "Ring should not be null";
+            throw new NullPointerException(excMsg);
+        }
         if (!(ring instanceof ImaginaryQuadraticRing)) {
             String excMsg = "Ring is not imaginary as needed";
             throw new IllegalArgumentException(excMsg);
