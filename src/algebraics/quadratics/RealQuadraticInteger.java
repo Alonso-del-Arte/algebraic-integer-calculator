134f5e077075f42c94be4c70f3be46c58ae9456d
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-23 19:59:46 -0500

    Rewind toStringAlt( ) to failing

--- src/algebraics/quadratics/RealQuadraticInteger.java
+++ src/algebraics/quadratics/RealQuadraticInteger.java
@@ -38,6 +38,11 @@ public class RealQuadraticInteger extends QuadraticInteger
     private final double numVal;
     private final double absNumVal;
     
+    @Override
+    public String toStringAlt() {
+        return "REWIND TO FAILING";
+    }
+    
     /**
      * Gives the absolute value of the numeric value of this real quadratic 
      * integer. In most cases it will be a rational approximation.
