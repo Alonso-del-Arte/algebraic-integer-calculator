bc3eb136229bd6c0df901485b8a340a65f316b72
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-30 23:26:57 -0500

    Passing toStringAlt( ) test for negative theta

--- src/algebraics/quadratics/QuadraticInteger.java
+++ src/algebraics/quadratics/QuadraticInteger.java
@@ -373,7 +373,11 @@ public abstract class QuadraticInteger implements AlgebraicInteger,
     
     public String toStringAlt() {
         if (this.quadRing.d1mod4) {
-            return Character.toString(THETA_LETTER);
+            if (this.regPartMult == 1) {
+                return Character.toString(THETA_LETTER);
+            } else {
+                return new String(new char[]{MINUS_SIGN, THETA_LETTER});
+            }
         } else {
             return this.toString();
         }
