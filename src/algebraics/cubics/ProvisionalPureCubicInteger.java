f39d3d468c66112614410c57065bb666f5572896
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-27 23:07:21 -0400

    New class, provisional

---
+++ src/algebraics/cubics/ProvisionalPureCubicInteger.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package algebraics.cubics;
+
+import fractions.Fraction;
+
+/**
+ * Draft class for PureCubicInteger. Normally I would do this sort of work in a 
+ * file not checked into version control. But as I expect this work to take me a 
+ * long time, I decided it would be better to have this checked into version 
+ * control.
+ * @author Alonso del Arte
+ */
+// TODO: Mark @Deprecated once all useful material is copied over to 
+// PureCubicInteger
+public class ProvisionalPureCubicInteger extends CubicInteger {
+    
+    // TODO: Write tests for this
+    @Override
+    public int algebraicDegree() {
+        return -1;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long trace() {
+        return 0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long norm() {
+        return 0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public long[] minPolynomialCoeffs() {
+        long[] array = {0, 0, 0};
+        return array;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialStringTeX() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String minPolynomialStringHTML() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String toASCIIString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String toTeXString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public String toHTMLString() {
+        return "SORRY, NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double abs() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double getRealPartNumeric() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double getImagPartNumeric() {
+        return 0.0;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public boolean isReApprox() {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public boolean isImApprox() {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    @Override
+    public double angle() {
+        return 0.0;
+    }
+        
+    // TODO: Write tests for this
+    public ProvisionalPureCubicInteger(int a, int b, int c, CubicRing ring) {
+        super(ring);
+    }
+    
+    // TODO: Write tests for this
+    public ProvisionalPureCubicInteger(Fraction a, Fraction b, Fraction c, 
+            CubicRing ring) {
+        super(ring);
+    }
+    
+}
---
+++ test/algebraics/cubics/ProvisionalPureCubicIntegerTest.java
@@ -0,0 +1,260 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package algebraics.cubics;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import fractions.Fraction;
+
+/**
+ * Tests of the ProvisionalPureCubicInteger class. Normally I would do this sort 
+ * of work in a file not checked into version control. But as I expect this work 
+ * to take me a long time, I decided it would be better to have this checked 
+ * into version control.
+ * @author Alonso del Arte
+ */
+public class ProvisionalPureCubicIntegerTest {
+    
+    public ProvisionalPureCubicIntegerTest() {
+    }
+
+    /**
+     * Test of algebraicDegree method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testAlgebraicDegree() {
+        System.out.println("algebraicDegree");
+        ProvisionalPureCubicInteger instance = null;
+        int expResult = 0;
+        int result = instance.algebraicDegree();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of trace method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testTrace() {
+        System.out.println("trace");
+        ProvisionalPureCubicInteger instance = null;
+        long expResult = 0L;
+        long result = instance.trace();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of norm method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testNorm() {
+        System.out.println("norm");
+        ProvisionalPureCubicInteger instance = null;
+        long expResult = 0L;
+        long result = instance.norm();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of minPolynomialCoeffs method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testMinPolynomialCoeffs() {
+        System.out.println("minPolynomialCoeffs");
+        ProvisionalPureCubicInteger instance = null;
+        long[] expResult = null;
+        long[] result = instance.minPolynomialCoeffs();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of minPolynomialString method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testMinPolynomialString() {
+        System.out.println("minPolynomialString");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.minPolynomialString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of minPolynomialStringTeX method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testMinPolynomialStringTeX() {
+        System.out.println("minPolynomialStringTeX");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.minPolynomialStringTeX();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of minPolynomialStringHTML method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testMinPolynomialStringHTML() {
+        System.out.println("minPolynomialStringHTML");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.minPolynomialStringHTML();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toASCIIString method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testToASCIIString() {
+        System.out.println("toASCIIString");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.toASCIIString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toTeXString method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testToTeXString() {
+        System.out.println("toTeXString");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.toTeXString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toHTMLString method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testToHTMLString() {
+        System.out.println("toHTMLString");
+        ProvisionalPureCubicInteger instance = null;
+        String expResult = "";
+        String result = instance.toHTMLString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of abs method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testAbs() {
+        System.out.println("abs");
+        ProvisionalPureCubicInteger instance = null;
+        double expResult = 0.0;
+        double result = instance.abs();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRealPartNumeric method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testGetRealPartNumeric() {
+        System.out.println("getRealPartNumeric");
+        ProvisionalPureCubicInteger instance = null;
+        double expResult = 0.0;
+        double result = instance.getRealPartNumeric();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getImagPartNumeric method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testGetImagPartNumeric() {
+        System.out.println("getImagPartNumeric");
+        ProvisionalPureCubicInteger instance = null;
+        double expResult = 0.0;
+        double result = instance.getImagPartNumeric();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isReApprox method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testIsReApprox() {
+        System.out.println("isReApprox");
+        ProvisionalPureCubicInteger instance = null;
+        boolean expResult = false;
+        boolean result = instance.isReApprox();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isImApprox method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testIsImApprox() {
+        System.out.println("isImApprox");
+        ProvisionalPureCubicInteger instance = null;
+        boolean expResult = false;
+        boolean result = instance.isImApprox();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of angle method, of class ProvisionalPureCubicInteger.
+     */
+    @Test
+    public void testAngle() {
+        System.out.println("angle");
+        ProvisionalPureCubicInteger instance = null;
+        double expResult = 0.0;
+        double result = instance.angle();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
