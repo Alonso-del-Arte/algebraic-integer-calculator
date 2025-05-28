10d51ed57c11a0fced9b4860b661b5789fcb86da
Alonso del Arte <alonso.delarte@gmail.com>
2025-05-28 00:07:29 -0400

    Back to passing getCubeRoot( ) test

--- src/algebraics/cubics/PureCubicRing.java
+++ src/algebraics/cubics/PureCubicRing.java
@@ -52,7 +52,7 @@ public class PureCubicRing extends CubicRing {
     }
 
     public double getCubeRoot() {
-        return Math.cbrt(this.radicand) + 3;
+        return Math.cbrt(this.radicand);
     }
 
     // TODO: Write tests for this
