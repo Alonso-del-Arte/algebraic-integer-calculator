87025393acb9d273e73327c9d3060f153930b8f1
Alonso del Arte <alonso.delarte@gmail.com>
2025-08-25 09:57:43 -0400

    Passing toASCIIStringAlt( ) test for purely real with d = 3 mod 4

--- src/algebraics/quadratics/ImaginaryQuadraticInteger.java
+++ src/algebraics/quadratics/ImaginaryQuadraticInteger.java
@@ -160,7 +160,8 @@ public class ImaginaryQuadraticInteger extends QuadraticInteger {
     
     @Override
     public String toASCIIStringAlt() {
-        if (this.quadRing.radicand == -1 || this.quadRing.radicand % 4 == -2) {
+        if (this.quadRing.radicand == -1 || this.quadRing.radicand % 4 == -2 
+                || this.quadRing.radicand % 4 == -1) {
             return Integer.toString(this.regPartMult)
                     .replace('-', MINUS_SIGN_CHARACTER);
         }
