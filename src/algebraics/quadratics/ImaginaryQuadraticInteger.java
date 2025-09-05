04670f88580f122f3af8065f870861708404125d
Alonso del Arte <alonso.delarte@gmail.com>
2025-09-04 23:30:52 -0400

    Passing toASCIIStringAlt( ) test for 0 in Eisenstein context

--- src/algebraics/quadratics/ImaginaryQuadraticInteger.java
+++ src/algebraics/quadratics/ImaginaryQuadraticInteger.java
@@ -161,7 +161,7 @@ public class ImaginaryQuadraticInteger extends QuadraticInteger {
     @Override
     public String toASCIIStringAlt() {
         if (this.quadRing.d1mod4) {
-            if (this.regPartMult > 0 && this.surdPartMult == 0 
+            if (this.regPartMult > -1 && this.surdPartMult == 0 
                     && this.quadRing.radicand == -3) {
                 return Integer.toString(this.regPartMult);
             }
