9be2ee53a0e63f622cbdeab951d5fbf7416bbb00
Alonso del Arte <alonso.delarte@gmail.com>
2025-10-09 00:17:39 -0400

    Changed isReApprox( ) stub to fail the next test

--- src/algebraics/cubics/ProvisionalPureCubicInteger.java
+++ src/algebraics/cubics/ProvisionalPureCubicInteger.java
@@ -147,7 +147,7 @@ public class ProvisionalPureCubicInteger extends CubicInteger {
     // TODO: Write tests for this
     @Override
     public boolean isReApprox() {
-        return false;
+        return true;
     }
     
     /**
