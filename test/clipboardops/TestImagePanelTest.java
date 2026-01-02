49337174549a328e1bc7723a89a03e436b39088e
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-01 23:44:18 -0500

    New test class

---
+++ test/clipboardops/TestImagePanelTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2026 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package clipboardops;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Tests of the TestImagePanel class.
+ * @author Alonso del Arte
+ */
+public class TestImagePanelTest {
+    
+}
